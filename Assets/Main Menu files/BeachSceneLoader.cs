a0a05ff2a87b1068df092d76b613942797e3df9f
mo <morganmccloskey@lcad.edu>
2026-09-24 03:20:22 -0700

    attached level 2 to button
    level 2 button works

--- Assets/Main Menu files/BeachSceneLoader.cs
+++ Assets/Main Menu files/BeachSceneLoader.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BeachSceneLoader : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnClick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("seal maze game");
     }
 }
