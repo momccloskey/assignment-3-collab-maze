089222b257b2300471cfe7ffbd9b88a3c8679d4c
henriesarts <henrymcmanus@lcad.edu>
2026-09-23 22:06:57 -0700

    Scene Transitions and Quit Game
    Added scene transition script to henry forest level when level 1 button is pressed as well as a quit game script

---
+++ Assets/Main Menu files/ForestSceneLoader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ForestSceneLoader : MonoBehaviour
+{
+    public void LoadForest()
+    {
+        SceneManager.LoadScene("forest maze");
+    }
+
+}
+
+
+  
+
